5371ee97f4dec8c9a6fe0efc1ca6256a73a8df70
zwiks - omen <stanislaw.debicki@proton.me>
2026-06-22 21:47:19 +0200

    feat: frontend scuffolding #T2.1

---
+++ frontend/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { Canvas } from "@threlte/core";
+    import GameScene from "./scenes/GameScene.svelte";
+  
+</script>
+
+
+<div>
+  <Canvas>
+    <GameScene/>
+  </Canvas>
+</div>
+<h1>Hell world</h1>
